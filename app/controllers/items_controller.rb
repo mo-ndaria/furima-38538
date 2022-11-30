9f373a45265d977e1011cc3f531844f343d0b8f3
mo-ndaria <maurakai2663@gmail.com>
2022-11-30 20:43:02 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-before_action :set_item, only:[:show]
+before_action :set_item, only:[:show, :edit, :update]
 before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -21,6 +21,24 @@ before_action :authenticate_user!, except: [:index, :show]
 
   def show
   end
+
+  def edit
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+  end
+
+
 
   private
 
