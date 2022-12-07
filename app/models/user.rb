d34eff8c9fa434d8bde054431263bd3e8aaf1719
mo-ndaria <maurakai2663@gmail.com>
2022-12-07 16:58:52 +0900

    商品購入機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   validates :birthday,presence: true
 
   has_many :items
+  has_many :orders
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
